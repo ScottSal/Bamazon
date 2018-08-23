b6f1fb90e0d82c9487b43ed40a8b580e5897b545
Scott Saloom <scottsaloom@gmail.com>
2018-08-23 17:31:19 -0500

    Removed $

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -12,31 +12,31 @@ CREATE TABLE Products(
 );
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("MacBook Pro 15inch", "Electronics", $3000.84, 10);
+VALUES ("MacBook Pro 15inch", "Electronics", 3000.84, 10);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("55 inch Flatscreen TV", "Electronics", $1200.67, 50);
+VALUES ("55 inch Flatscreen TV", "Electronics", 1200.67, 50);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("Gray Sweatshirt", "Clothing", $15.99, 100);
+VALUES ("Gray Sweatshirt", "Clothing", 15.99, 100);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("Windshield Wipers", "Auto Parts", $20.75, 200);
+VALUES ("Windshield Wipers", "Auto Parts", 20.75, 200);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("Monopoly", "Toys", $17.99, 95);
+VALUES ("Monopoly", "Toys", 17.99, 95);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("Happy Gilmore", "Movies", $10.99, 30);
+VALUES ("Happy Gilmore", "Movies", 10.99, 30);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("Nike Tennis Shoes", "Clothing", $75.87, 25);
+VALUES ("Nike Tennis Shoes", "Clothing", 75.87, 25);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("Teddy Bear", "Toys", $20.99, 33);
+VALUES ("Teddy Bear", "Toys", 20.99, 33);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("Avengers: Infinity War", "Movies", $22.99, 6);
+VALUES ("Avengers: Infinity War", "Movies", 22.99, 6);
 
 INSERT INTO Products (product_name, department_name, price, stock_quantity)
-VALUES ("Mystery Box", "???", $5000, 1);
+VALUES ("Mystery Box", "???", 5000, 1);
